d2b87fbdb4dc1db2f6ed4755bb6ba18d5b37caef
YunJin Choi <yunjinchoi89@gmail.com>
2020-09-04 13:50:46 +0900

    02-02 초기커밋 및 gitignore 추가

---
+++ 02/01/Ruliweb.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+from package.CrawlingPackage import CrawlingPackage
+
+class Ruliweb:
+    
+    def __init__(self):
+
+
+
+if __name__ == "__main__":
+
+
