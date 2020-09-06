8493214251a9f8e511894b1034107e3dc2d9d8dc
YunJin Choi <yunjinchoi89@gmail.com>
2020-09-06 14:00:43 +0900

    02-01 Ruliweb login 완성

--- 02/01/Ruliweb.py
+++ 02/01/Ruliweb.py
@@ -12,9 +12,10 @@ class Ruliweb:
 
     def login(self):
         url = 'https://user.ruliweb.com/member/login_proc'
-        acct = pd.read_csv('account.scrt')
+        acct = pd.read_csv('D:\\03.Code\\06.python_study\\python_study\\02\\01\\account.scrt')
         data = {'user_id' : acct['ID'], 'user_pw' : acct['PW']}
-        self.libs.sess.post(url, data=data)
+        res = self.libs.sess.post(url, data=data)
+        print('로그인 성공') if res.ok else print('로그인 실패')
     
     def setBaseUrl(self, url):
         self.base_url = url
